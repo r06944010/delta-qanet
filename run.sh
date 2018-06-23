78c818786946c0b795a97c8526b33254fa7a14b0
r06944010 <r06944010@ntu.edu.tw>
2018-06-23 19:46:03 +0800

    new

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+NOW=$(date +"%m%d-%R")
 echo "mode : $1"
 if [[ $1 = "train" ]];then
-    CUDA_VISIBLE_DEVICE=3 python config.py --mode train
+    CUDA_VISIBLE_DEVICES=1 python config.py --mode train | tee log/$NOW
 elif [[ $1 = "debug" ]];then
-    CUDA_VISIBLE_DEVICE=3 python config.py --mode debug
+    CUDA_VISIBLE_DEVICES=1 python config.py --mode debug | tee log/$NOW
 fi
